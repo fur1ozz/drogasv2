e53051792a8558a6cd5490369d411cf5fecde62d
fur1ozz <tomassveveris@gmail.com>
2023-11-28 09:17:04 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
       <Router>
         <div className="min-h-screen dark:bg-[#1d2125]">
           <Routes>
-            <Route exact path="/Home" element = {<Home />}/>
+            <Route exact path="/Home" element = {<HomePage />}/>
           </Routes>
         </div>
       </Router>
