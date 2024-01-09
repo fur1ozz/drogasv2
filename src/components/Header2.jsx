61e87fe6408a70f24f1381b051b3736a26d8c75f
fur1ozz <tomassveveris@gmail.com>
2024-01-09 10:08:41 +0200

    last small changes

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -13,6 +13,7 @@ function Header2(){
     };
 
     const access = localStorage.getItem("access");
+    const logedUserName = localStorage.getItem("username");
 
     return(
       <>
@@ -34,7 +35,12 @@ function Header2(){
                                       </svg>
                                   )}
                               </button>
-                          <Link to="/Login" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Login</Link>
+                          <Link
+                              to="/Login"
+                              className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                          >
+                              Login
+                          </Link>
                           <button
                               onClick={handleMobileMenuToggle}
                               type="button"
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
                 alt="Background Image"
                 className="w-full h-full object-cover absolute z-0"
             />
-            <div className="absolute top-1/2 left-1/4 transform -translate-y-1/2 text-white z-10 w-[500px]">
+            <div className="absolute top-1/2 left-1/4 transform -translate-y-1/2 text-white z-10 w-full max-w-[500px] sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%]">
                 <h1 className="text-4xl font-bold mb-4">Welcome to Drogas</h1>
                 <p className="text-lg">
                     Welcome to the ultimate online destination – where quality meets convenience, and your shopping desires come to life!                </p>
